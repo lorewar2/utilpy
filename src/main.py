ca3bb80887d96311a6c1414d82f573692239a904
lorewar2 <mw8511@live.com>
2024-02-27 15:34:22 -0600

    read vcf

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ HERA1_REF_LOC = "/data1/phasstphase_test/potato/k_mer_plot_stuff/parent_ref/hera
 HERA2_REF_LOC = "/data1/phasstphase_test/potato/k_mer_plot_stuff/parent_ref/hera2/solTubHeraHap2.fa"
 STIEG1_REF_LOC = "/data1/phasstphase_test/potato/k_mer_plot_stuff/parent_ref/stieg1/solTubStiegHap1.fa"
 STIEG2_REF_LOC = "/data1/phasstphase_test/potato/k_mer_plot_stuff/parent_ref/stieg2/solTubStiegHap2.fa"
+VCF_LOC = "/data1/phasstphase_test/potato/hifi/potato_deep.vcf"
 INTERMEDIATE_LOC = "./intermediate/"
 K = 30
 
@@ -19,7 +20,8 @@ def main():
     # search the vcf and find a kmer
 
     # look for the kmer in all 4 parents
-    util.search_for_kmer_in_intermediate(TABEX_LOC, INTERMEDIATE_LOC, HERA1_REF_LOC)
+    #util.search_for_kmer_in_intermediate(TABEX_LOC, INTERMEDIATE_LOC, HERA1_REF_LOC, "aaaaaaatatttagtggtgataaattttct")
+    util.open_vcf_and_get_k_mer(K, VCF_LOC, HERA1_REF_LOC)
     return
 
 if __name__ == "__main__":
--- src/util.py
+++ src/util.py
@@ -1,5 +1,7 @@
 import os
-# todo1
+import vcf
+import pyfaidx
+
 def run_fastk_make_intermediate_files(k, fast_k_loc, intermediate_loc, ref_loc):
     # get the file name from path
     file_name = ref_loc.split("/")[-1]
@@ -10,17 +12,48 @@ def run_fastk_make_intermediate_files(k, fast_k_loc, intermediate_loc, ref_loc):
     print(os.popen(command_to_run).read())
     return
 
-# todo2
-def open_vcf_and_get_k_mer():
-
+def open_vcf_and_get_k_mer(k, vcf_loc, ref_loc):
+    ref_kmer_list = []
+    alt_kmer_list = []
+    variant_reader = vcf.Reader(file=vcf_loc)
+    ref_fasta = pyfaidx.Fasta(ref_loc)
+    if k % 2 == 0:
+        k_first_half_length = (k // 2) + 1
+        k_second_half_length = k // 2
+    else:
+        k_first_half_length = k // 2
+        k_second_half_length = k // 2
+    for record in variant_reader:
+        if len(record.alleles) > 2:
+            continue
+        ref = record.alleles[0]
+        alt = record.alleles[1]
+        try:
+            kmer_first_half = ref_fasta[record.CHROM][record.POS - k_first_half_length : record.POS]
+            kmer_second_half = ref_fasta[record.CHROM][record.POS + len(ref) : record.POS + k_second_half_length]
+            kmer_second_half_alt = ref_fasta[record.CHROM][record.POS + len(ref) : record.POS + len(ref) - len(alt) + k_second_half_length + 1]
+            ref_kmer = kmer_first_half + ref + kmer_second_half
+            alt_kmer = kmer_first_half + alt + kmer_second_half_alt
+            print(ref_kmer)
+            print(alt_kmer)
+            ref_kmer_list.append(ref_kmer)
+            alt_kmer_list.append(alt_kmer)
+        except:
+            pass
+        break
     return
 
-# todo3
-def search_for_kmer_in_intermediate(tabex_loc, intermediate_loc, ref_loc):
+def search_for_kmer_in_intermediate(tabex_loc, intermediate_loc, ref_loc, k_string):
     file_name = ref_loc.split("/")[-1]
     ktab_path = "{}{}.ktab".format(intermediate_loc, file_name)
-    command_to_run = "{} {} LIST".format(tabex_loc, ktab_path, ref_loc)
+    command_to_run = "{} {} {} {}".format(tabex_loc, ktab_path, ref_loc, k_string)
     print(command_to_run)
+    output = os.popen(command_to_run).read()
+    if output.find("Not Found") == -1:
+        print("KMER DOESNT EXIST")
+    else:
+        print("KMER EXIST")
     return
 
- #./FastK -k30 -N"./../" -T64 /data1/phasstphase_test/potato/reference/solTubHeraHap1.fa
+#./FastK -k30 -N"./../" -T64 /data1/phasstphase_test/potato/reference/solTubHeraHap1.fa
+# aaaaaaatatttagtggtgataaattttct
