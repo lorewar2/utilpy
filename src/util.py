166167dd41ec58ba096e2fa0f50bba8650312f85
lorewar2 <mw8511@live.com>
2024-02-27 17:47:00 -0600

    bug fix

--- src/util.py
+++ src/util.py
@@ -63,7 +63,7 @@ def find_which_parent_contain_kstring(k_string_vec, tabex_loc, intermediate_loc,
 def search_for_kstring_in_intermediate(tabex_loc, intermediate_loc, ref_loc, k_string):
     file_name = ref_loc.split("/")[-1]
     ktab_path = "{}{}.ktab".format(intermediate_loc, file_name)
-    command_to_run = "{} {} {} {}".format(tabex_loc, ktab_path, k_string)
+    command_to_run = "{} {} {}".format(tabex_loc, ktab_path, k_string)
     print(command_to_run)
     output = os.popen(command_to_run).read()
     if output.find("Not found") == -1:
