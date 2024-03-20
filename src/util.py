5637d0dd08685e251fddd35bbbaf09e310a742d7
lorewar2 <mw8511@live.com>
2024-03-20 18:40:53 -0500

    output modified

--- src/util.py
+++ src/util.py
@@ -91,10 +91,6 @@ def open_vcf_and_get_k_mer(k, vcf_loc, ref_loc):
             haplotype_alleles_list.append(record.samples[0]["GT"])
             ref_alt_kmer_list.append((ref_kmer, alt_kmer))
             ref_location_list.append((record.CHROM, record.POS))
-            # test just 100 or specified iterations
-            if index > 100:
-                break
-            #print(ref_kmer + "appended")
     return ref_alt_kmer_list, haplotype_alleles_list, ref_location_list, phase_block_numbers
 
 def find_which_parent_contain_kstring(thread_index, k_string_vec, haplotype_allele_vec, ref_loc_vec, phase_blocks, tabex_loc, intermediate_loc, hera_ref, stieg_ref):
@@ -103,8 +99,10 @@ def find_which_parent_contain_kstring(thread_index, k_string_vec, haplotype_alle
     concancated_alt_k_string = ""
     for k_index, k_string in enumerate(k_string_vec):
         ref_k_string, alt_k_string = k_string
-        print(k_index, phase_blocks[k_index])
+        #print(k_index, phase_blocks[k_index])
         # run tabx when 100 k strings are collected
+        if k_index % 1000 == 0:
+            print("Thread {} progress {}%".format(thread_index, 100 * k_index / len(k_string_vec)))
         if (k_index % 20 == 0) and (k_index != 0):
             hera_ref_result = search_for_kstring_in_intermediate(tabex_loc, hera_ref, concancated_ref_k_string)
             stieg_ref_result = search_for_kstring_in_intermediate(tabex_loc, stieg_ref, concancated_ref_k_string)
@@ -115,12 +113,12 @@ def find_which_parent_contain_kstring(thread_index, k_string_vec, haplotype_alle
             # process the stuff put in appropriate phase block and haplotype (increment)
             local_i = 0
             for global_i in range(k_index - 20, k_index):
-                print(local_i, global_i)
-                print("Reference location: {}".format(ref_loc_vec[global_i]))
-                print("Haplotype: {}".format(haplotype_allele_vec[global_i]))
-                print("Phase block number: {}".format(phase_blocks[global_i]))
-                print("Exclusive Ref kmer found: hera {} stieg {}".format(hera_ref_result[local_i], stieg_ref_result[local_i]))
-                print("Exclusive Alt kmer found: hera {} stieg {}".format(hera_alt_result[local_i], stieg_alt_result[local_i]))
+                #print(local_i, global_i)
+                #print("Reference location: {}".format(ref_loc_vec[global_i]))
+                #print("Haplotype: {}".format(haplotype_allele_vec[global_i]))
+                #print("Phase block number: {}".format(phase_blocks[global_i]))
+                #print("Exclusive Ref kmer found: hera {} stieg {}".format(hera_ref_result[local_i], stieg_ref_result[local_i]))
+                #print("Exclusive Alt kmer found: hera {} stieg {}".format(hera_alt_result[local_i], stieg_alt_result[local_i]))
                 # make a new block if the phase block is different
                 if phase_blocks[global_i] != final_result_blocks[-1][0]:
                     print("New phase block processing.. {}".format(phase_blocks[global_i]))
@@ -188,7 +186,7 @@ def find_which_parent_contain_kstring(thread_index, k_string_vec, haplotype_alle
     write_path = "{}final_result_{}.txt".format(intermediate_loc, thread_index)
     with open(write_path, 'a') as fw:
         for block in final_result_blocks:
-            fw.write("{}".format(block))
+            fw.write("{}\n".format(block))
     return
 
 def search_for_kstring_in_intermediate(tabex_loc, ref_loc, k_string):
