afb9074729227630dfa734e905f2361e12af434e
lorewar2 <mw8511@live.com>
2024-08-21 17:46:00 -0500

    find kmers in phase block

--- src/main.py
+++ src/main.py
@@ -21,13 +21,14 @@ K = 21
 
 def main():
     # make intermediate files for merged hera and merged stieg
-    util.run_fastk_make_intermediate_files_from_reads(K, FASTK_LOC, INTERMEDIATE_LOC, HERA_READ_LOC, 6)
-    util.run_fastk_make_intermediate_files_from_reads(K, FASTK_LOC, INTERMEDIATE_LOC, STIEG_READ_LOC, 5)
+    #util.run_fastk_make_intermediate_files_from_reads(K, FASTK_LOC, INTERMEDIATE_LOC, HERA_READ_LOC, 6)
+    #util.run_fastk_make_intermediate_files_from_reads(K, FASTK_LOC, INTERMEDIATE_LOC, STIEG_READ_LOC, 5)
     # make unique kmer files
-    util.unique_kmers_for_parent_from_intermediates_from_reads(LOGEX_LOC, INTERMEDIATE_LOC, HERA_READ_LOC, STIEG_READ_LOC)
+    #util.unique_kmers_for_parent_from_intermediates_from_reads(LOGEX_LOC, INTERMEDIATE_LOC, HERA_READ_LOC, STIEG_READ_LOC)
     # search the vcf and find a kmer
     #k_string_vec, haplotype_allele_vec, ref_loc_vec, phase_blocks = util.open_vcf_and_get_k_mer(K, VCF_LOC, HERA1_REF_LOC)
     # look for the kmer in all 4 parents
+    util.find_specific_phaseblock_kmer(K, VCF_LOC, HERA1_REF_LOC, 3224262, HERA_UNIQUE_LOC, STIEG_UNIQUE_LOC)
     #util.find_which_parent_contain_kstring(0, k_string_vec, haplotype_allele_vec, ref_loc_vec, phase_blocks, TABEX_LOC, INTERMEDIATE_LOC, HERA_UNIQUE_LOC, STIEG_UNIQUE_LOC)
     return
 
