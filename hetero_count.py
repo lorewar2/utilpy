f64f9e442ba8c8d40ccb75307d0110d5581ce729
minindu0 <mw8511@live.com>
2024-09-05 14:09:00 -0500

    count the phased and total

---
+++ hetero_count.py
@@ -0,0 +1,32 @@
+# Using readlines()
+file1 = open('hiphase_modified.vcf', 'r')
+Lines = file1.readlines()
+count = 0
+phased_count = 0
+all_count = 0
+# Strips the newline character
+for line in Lines:
+    count += 1
+    if count % 1000 == 0:
+        print(count)
+    split_array = line.strip().split("\t")
+    if split_array[0][0] == "c":
+        #print("yes")
+        #print(split_array[9])
+        # extract the three points
+        haplotype = split_array[9].split(":")[0]
+        assert(len(haplotype) == 3)
+        ps_available = False
+        for entry in split_array[8].split(":"):
+            if entry == "PS":
+                ps_available = True
+        if haplotype == "0|1" or haplotype == "1|0" or haplotype == "0/1" or haplotype == "1/0":
+            all_count += 1
+            if ps_available:
+                phased_count += 1
+
+print("all count", all_count)
+print("phased count", phased_count)
+
+
+
