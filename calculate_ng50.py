eff023471ff6fa9923485d68ba101918fb5de7d5
lorewar2 <mw8511@live.com>
2024-09-10 17:49:49 -0500

    update ng50

--- calculate_ng50.py
+++ calculate_ng50.py
@@ -1,19 +1,20 @@
 def main():
     genome_length = 3_200_000_000
-    file1 = open('/data1/phasstphase_test/hg38/hapcut2_output/hapcut2_out.phased.VCF', 'r')
+    file1 = open('phasstphase_modified.vcf', 'r')
     Lines = file1.readlines()
     phase_blocks = []
     count = 0
     current_phase_block = 0
     current_phase_block_length = 0
+    current_phase_block_start = 0
     # Strips the newline character
     for line in Lines:
         split_array = line.strip().split("\t")
-        location = int(split_array[1])
         count += 1
-        if count % 1000 == 0:
-            print(count, location)
+        if count % 5000 == 0:
+            print(count)
         if split_array[0][0] == "c":
+            location = int(split_array[1])
             haplotype = split_array[9].split(":")[0]
             assert(len(haplotype) == 3)
             ps_available = False
@@ -24,15 +25,21 @@ def main():
                     break
                 PS_pos += 1
             if ps_available:
+                if split_array[9].split(":")[PS_pos] == ".":
+                    continue
                 this_phase_block = int(split_array[9].split(":")[PS_pos])
                 if this_phase_block == current_phase_block:
-                    current_phase_block_length = location - this_phase_block
+                    current_phase_block_length = location - current_phase_block_start
+                    if current_phase_block_length > 100_000:
+                        print(location, this_phase_block, "dsd")
+                        print(line)
                 else:
-                    if current_phase_block_length != 0:
+                    if current_phase_block_length > 0:
                         phase_blocks.append(current_phase_block_length)
-                        current_phase_block = this_phase_block
-                        current_phase_block_length = 0
-
+                    current_phase_block = this_phase_block
+                    current_phase_block_length = 0
+                    current_phase_block_start = location
+    print(phase_blocks)
     print(calculate_n50(phase_blocks))
     print(calculate_ng50(phase_blocks, genome_length))
     return
