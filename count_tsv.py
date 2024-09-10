56f1e6ebb4c9da065832705595aefe2cc7ad3173
lorewar2 <mw8511@live.com>
2024-09-10 15:30:57 -0500

    counter updated

--- count_tsv.py
+++ count_tsv.py
@@ -1,22 +1,29 @@
 # Using readlines()
-file1 = open('hiphase_eval.tsv', 'r')
+file1 = open('whatshap_modified.vcf', 'r')
 Lines = file1.readlines()
-
 count = 0
-switch_count = 0
-flip_count = 0
-total_variant = 0
-phased_variant = 0
+phased_count = 0
+all_count = 0
 # Strips the newline character
 for line in Lines:
     count += 1
-    if count > 1:
-        split_array = line.strip().split("\t")
-        switch_count += int(split_array[11].split("/")[0])
-        flip_count += int(split_array[11].split("/")[1])
-        total_variant += int(split_array[7])
-        phased_variant += int(split_array[8])
-print(switch_count)
-print(flip_count)
-print(total_variant)
-print(phased_variant)
+    if count % 1000 == 0:
+        print(count)
+    split_array = line.strip().split("\t")
+    if split_array[0][0] == "c":
+        #print("yes")
+        #print(split_array[9])
+        # extract the three points
+        haplotype = split_array[9].split(":")[0]
+        assert(len(haplotype) == 3)
+        ps_available = False
+        for entry in split_array[8].split(":"):
+            if entry == "PS":
+                ps_available = True
+        if haplotype == "0|1" or haplotype == "1|0" or haplotype == "0/1" or haplotype == "1/0":
+            all_count += 1
+            if ps_available:
+                phased_count += 1
+
+print("all count", all_count)
+print("phased count", phased_count)
